79a703956afca3727390931ba9732fc47528cc72
Storm Dragon <stormdragon2976@gmail.com>
2018-04-25 13:29:18 -0400

    Function to present input to users with a default choice option, and will loop til one of the offered coices is selected. Sort of like the select menu loop, but you can choose answers via y/n or words.

--- files/usr/lib/F123-includes/script_functions.sh
+++ files/usr/lib/F123-includes/script_functions.sh
@@ -22,6 +22,39 @@
 #
 #--code--
 
+get_input()
+{
+    # Get user input args are return variable, question, options
+    # Prefix an anser option with - to make it the default
+    local __get_input_input=$1
+    shift
+    local __get_input_question="$1"
+    shift
+    local __get_input_answer=""
+    local __get_input_i=""
+    local __get_input_continue=false
+    for __get_input_i in $@; do
+        if [[ "${__get_input_i:0:1}" == "-" ]]; then
+            local __get_input_default="${__get_input_i:1}"
+        fi
+    done
+    while [[ "$__get_input_continue" == "false" ]]; do
+        echo -n "$__get_input_question (${@/#-/}) "
+        if [[ -n "$__get_input_default" ]]; then
+            read -e -i "$__get_input_default" __get_input_answer
+        else
+            read -e __get_input_answer
+        fi
+        for __get_input_i in $@; do
+            if [[ "$__get_input_answer" == "${__get_input_i/#-/}" ]]; then
+                __get_input_continue=true
+                break
+            fi
+        done
+    done
+    eval $__get_input_input="'$__get_input_answer'"
+}
+
 get_keypress() {
     # Returnes the pressed key.
     # There arre two ways to use this function.
