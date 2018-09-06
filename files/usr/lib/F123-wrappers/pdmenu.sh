821fb396b5603d984ec29bcf93f8a8105d541d5c
Kyle <kyle@free2.ml>
2018-09-06 15:57:36 -0400

    Pdmenu: fixed capitalization and verbage, commented out some games that don't work with screen readers, removed tools menu and reorganized its contents in other menus.

--- files/usr/lib/F123-wrappers/pdmenu.sh
+++ files/usr/lib/F123-wrappers/pdmenu.sh
@@ -28,49 +28,64 @@ export TEXTDOMAINDIR=/usr/share/locale
 . gettext.sh
 
 cat << EOF
-title:$(gettext "Welcome to F123 Light")
+title:$(gettext "Welcome to F123Light")
 
 # Define the main menu.
 menu:main:$(gettext "F123 Light Main Menu"):$(gettext "Use the up and down arrow keys to select your choice and press the 'Enter' key to activate it.")
 	show:$(gettext "_Games Menu")..::games
-	show:$(gettext "_Internet menu")..::internet
-	show:$(gettext "_Media menu")..::media
-	show:$(gettext "_Office menu")..::office
-	show:$(gettext "_System Configuration Menu")..::configuration
-	show:$(gettext "_Tools Menu")..::tools
-	show:$(gettext "_Help Menu")..::help
-	nop:Search
+	show:$(gettext "_Internet Menu")..::internet
+	show:$(gettext "_Media Menu")..::media
+	show:$(gettext "_Office Menu")..::office
+	exec:$(gettext "File _Browser")::clear;mc -K /etc/mc/mc.keymap
+	group:$(gettext "Browse External Drives")
+		exec::makemenu: \
+		echo "menu:external:$(gettext "External"):$(gettext "Select Drive")"; \
+		rmdir /media/* &> /dev/null; \
+		c=0; \
+		for i in \$(find /media -maxdepth 1 ! -path /media -type d) ; do \
+			((c++)); \
+			j="\${i/\/media\//}"; \
+			echo "exec:_\$j::mc '\$i'"; \
+			echo "exec:$(gettext "Safely remove") _\$j::sudo umount '\$i'"; \
+		done; \
+		[[ \$c -gt 0 ]] && echo "exec:\$(gettext "No external drives found"):pause:\$(gettext "No external drives found")"; \
+		echo "exit:$(gettext "Back to Tools Menu").."
+		show:::external
+		remove:::external
+	endgroup
+	nop:$(gettext "Search")
     exec:$(gettext "_Find things on your computer"):edit,pause:recoll -t ~Search for what? :~
     exec:$(gettext "_Search the internet"):edit,pause:google ~Search for what? :~
 	nop
-	show:$(gettext "_Power Options")..::power
+	show:$(gettext "_Settings Menu")..::settings
+	show:$(gettext "_Help Menu")..::help
+	show:$(gettext "Turn Off or Restart _Computer")..::power
 	nop
-	exit:$(gettext "E_xit Menu")
-
+	exit:$(gettext "E_xit to Command Line")
 
 # Submenu for games.
 menu:games:$(gettext "Games"):$(gettext "Command line games")
 	exec:$(gettext "_Adventure")::clear;adventure
-	exec:$(gettext "_Arithmetic")::clear;arithmetic
-	exec:$(gettext "_Air Traffic Controler (Not screen reader friendly)")::clear;atc
-	exec:$(gettext "_Backgammon (Not screen reader friendly)")::clear;backgammon
+	exec:$(gettext "_Arithmetic Challenge!")::clear;arithmetic
+	#exec:$(gettext "_Air Traffic Controler (Not screen reader friendly)")::clear;atc
+	#exec:$(gettext "_Backgammon (Not screen reader friendly)")::clear;backgammon
 	exec:$(gettext "_Battlestar")::clear;battlestar
-	exec:$(gettext "_Boggle (Not screen reader friendly)")::clear;boggle
-	exec:$(gettext "_Canfield (Not screen reader friendly)")::clear;canfield
-	exec:$(gettext "_Cribbage (Not screen reader friendly)")::clear;cribbage
+	#exec:$(gettext "_Boggle (Not screen reader friendly)")::clear;boggle
+	#exec:$(gettext "_Canfield (Not screen reader friendly)")::clear;canfield
+	#exec:$(gettext "_Cribbage (Not screen reader friendly)")::clear;cribbage
 	exec:$(gettext "_Go Fish"):pause:clear;go-fish
 	exec:$(gettext "_Gomoku")::clear;gomoku
 	exec:$(gettext "_Hangman")::clear;hangman
-	exec:$(gettext "_Hunt (Not screen reader friendly)")::clear;hunt
+	#exec:$(gettext "_Hunt (Not screen reader friendly)")::clear;hunt
 	exec:$(gettext "_Mille Bornes")::clear;mille
 	exec:$(gettext "_Number")::clear;number
 	exec:$(gettext "_Phantasia")::clear;phantasia
 	exec:$(gettext "_Phase of the Moon"):pause:clear;pom
 	exec:$(gettext "_Primes")::clear;primes
-	exec:$(gettext "_Robots (Not screen reader friendly)")::clear;robots
+	#exec:$(gettext "_Robots (Not screen reader friendly)")::clear;robots
 	exec:$(gettext "_Sail")::clear;sail
-	exec:$(gettext "_Snake (Not screen reader friendly)")::clear;snake
-	exec:$(gettext "_Tetris (Not screen reader friendly)")::clear;tetris-bsd
+	#exec:$(gettext "_Snake (Not screen reader friendly)")::clear;snake
+	#exec:$(gettext "_Tetris (Not screen reader friendly)")::clear;tetris-bsd
 	exec:$(gettext "_Trek")::clear;trek
 	group:$(gettext "_Tux Math")
         exec:::clear
@@ -84,16 +99,14 @@ menu:games:$(gettext "Games"):$(gettext "Command line games")
         exec:::startx /usr/lib/F123-wrappers/tuxtype --tts
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
-	exec:$(gettext "_Worm (Not screen reader friendly)")::clear;worm
+	#exec:$(gettext "_Worm (Not screen reader friendly)")::clear;worm
 	exec:$(gettext "_Wumpus")::clear;wump
-
 	nop
-	exit:$(gettext "Main menu")..
-
+	exit:$(gettext "Main Menu")..
 
 # submenu for internet applications.
 menu:internet:$(gettext "Internet"):$(gettext "Internet programs")
-	group:$(gettext "Email (Thunderbird)")
+	group:$(gettext "E-mail (T_hunderbird)")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher thunderbird
@@ -101,27 +114,27 @@ menu:internet:$(gettext "Internet"):$(gettext "Internet programs")
     endgroup
 	nop:$(gettext "Web Browsers")
 	exec:$(gettext "_Basic Web Browser (W3M)")::clear;w3m
-	group:$(gettext "Full _Web browser (Firefox)")
+	group:$(gettext "_Full Web Browser (Firefox)")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher firefox
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
 	nop:$(gettext "Communication")
-	group:$(gettext "Instant Messenger (_Pidgin)")
+	group:$(gettext "_Text Chat (Pidgin)")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher pidgin
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
-	group:$(gettext "Voice Chat (_Mumble)")
+	group:$(gettext "_Voice Chat (Mumble)")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher mumble
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
 	nop
-	exit:$(gettext "_Main menu")..
+	exit:$(gettext "_Main Menu")..
 
 menu:media:$(gettext "Media"):$(gettext "Multi-media applications")
 	exec:$(gettext "_CD Audio Ripper (ripit)")::ripit
@@ -147,21 +160,20 @@ menu:media:$(gettext "Media"):$(gettext "Multi-media applications")
 		remove:::epub
 	endgroup
 	nop
-	exit:$(gettext "_Main menu")..
-
+	exit:$(gettext "_Main Menu")..
 
 menu:office:$(gettext "Office"):$(gettext "Word processing, calendar, etc")
 	exec:$(gettext "_Month Calendar"):pause:clear;ncal
 	exec:$(gettext "_Year Calendar"):pause:clear;ncal -y
-	exec:$(gettext "Text _Editor")::clear;${EDITOR:-nano}
+	exec:$(gettext "_Text Editor")::clear;${EDITOR:-nano}
 	nop:$(gettext "Office Suite")
-	group:$(gettext "Spreadsheet")
+	group:$(gettext "_Spreadsheet")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher localc
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
-	group:$(gettext "Word Processor")
+	group:$(gettext "_Word Processor")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher lowriter
@@ -174,55 +186,34 @@ menu:office:$(gettext "Office"):$(gettext "Word processing, calendar, etc")
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
 	nop
-	exit:$(gettext "_Main menu")..
+	exit:$(gettext "_Main Menu")..
 
 
 # submenu for configuring the computer.
-menu:configuration:$(gettext "Configuration"):$(gettext "System Configuration")
+menu:settings:$(gettext "Settings"):$(gettext "System configuration")
 	exec:$(gettext "_Change Passwords")::clear;/usr/lib/F123-wrappers/configure-passwords
-	exec:$(gettext "_Email Configuration")::clear;fleacollar
+	exec:$(gettext "_E-mail Configuration")::clear;fleacollar
 	exec:$(gettext "_Security Configuration")::clear;/usr/lib/F123-wrappers/configure-security.sh
-	exec:$(gettext "_Change System Speech")::clear;/usr/lib/F123-wrappers/configure-speech.sh
-	exec:$(gettext "_Change Soundcard")::clear;/usr/lib/F123-wrappers/configure-sound.sh
-	exec:$(gettext "_Wireless Internet Connection")::clear;sudo configure-wifi
-	nop
-	exit:$(gettext "_Main menu")..
-
-menu:tools:$(gettext "Tools"):$(gettext "System Tools")
-	exec:$(gettext "File _Browser")::clear;mc -K /etc/mc/mc.keymap
-	group:$(gettext "Browse External Drives")
-		exec::makemenu: \
-		echo "menu:external:$(gettext "External"):$(gettext "Select Drive")"; \
-		sudo rmdir /media/* &> /dev/null; \
-		c=0; \
-		for i in \$(find /media -maxdepth 1 ! -path /media -type d) ; do \
-			[[ -n "\$i" ]] && ((c++)); \
-			j="\${i/\/media\//}"; \
-			echo "exec:_\$j::mc '\$i'"; \
-			echo "exec:$(gettext "Safely remove") _\$j::sudo umount '\$i'"; \
-		done; \
-		[[ \$c -eq 0 ]] && echo "exec:\$(gettext "No external drives found"):pause:echo \"\$(gettext "No external drives found")\""; \
-		echo "exit:$(gettext "Back to Tools Menu").."
-		show:::external
-		remove:::external
-	endgroup
+	exec:$(gettext "Change System S_peech")::clear;/usr/lib/F123-wrappers/configure-speech.sh
+	exec:$(gettext "Change _Sound Output")::clear;/usr/lib/F123-wrappers/configure-sound.sh
 	group:$(gettext "Bluetooth manager")
         exec:::clear
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen
         exec:::startx /usr/lib/F123-wrappers/xlauncher blueman-assistant
         exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen
     endgroup
+	exec:$(gettext "Setup _Wifi")::clear;sudo configure-wifi
 	nop
-	exit:$(gettext "_Main menu")..
+	exit:$(gettext "_Main Menu")..
 
-menu:help:$(gettext "Get Help with F123 Light"):$(gettext "Get Help with F123 Light")
+menu:help:$(gettext "Get Help with F123 Light"):$(gettext "Get Help with F123Light")
 	exec:$(gettext "_Chat Bot")::tt++ /etc/chatbot.tin
 	nop
-	exit:$(gettext "_Main menu")..
+	exit:$(gettext "_Main Menu")..
 
-menu:power:$(gettext "Shutdown or Restart Your Computer"):$(gettext "Shutdown or restart your computer")
-	exec:$(gettext "_Power Off")::poweroff &> /dev/null || sudo poweroff
+menu:power:$(gettext "Turn off or Restart Computer"):$(gettext "Shutdown or restart your computer")
+	exec:$(gettext "Turn _Off")::poweroff &> /dev/null || sudo poweroff
 	exec:$(gettext "_Restart")::reboot &> /dev/null || sudo reboot
 	nop
-	exit:$(gettext "_Main menu")..
+	exit:$(gettext "_Main Menu")..
 EOF
