535c21f2f054818e0b552a455e0d16b79cc7a62b
Storm Dragon <stormdragon2976@gmail.com>
2018-10-08 12:31:00 -0400

    Launcher for selecting the correct mail client based on preferences.

---
+++ files/usr/lib/F123-wrappers/mail-launcher.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# mail-launcher.sh
+# Description
+#
+# Copyright 2018, F123 Consulting, <information@f123.org>
+# Copyright 2018, Storm Dragon, <storm_dragon@linux-a11y.org>
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+#--code--
+ 
+case "${preferences[mailClient]}" in
+    "mutt") command mutt;;
+    "thunderbird")
+        exec:::python /usr/share/fenrirscreenreader/tools/fenrir-ignore-screen &> /dev/null
+        exec:::echo -n "setting set screen#suspendingScreen=\$(</tmp/fenrirSuspend)" | socat - UNIX-CLIENT:/tmp/fenrirscreenreader-deamon.sock
+        exec:::command $([[ -n $DEMOMODE ]] && echo '-v') startx /usr/lib/F123-wrappers/xlauncher thunderbird
+        exec:::python /usr/share/fenrirscreenreader/tools/fenrir-unignore-screen &> /dev/null
+        exec:::echo -n "setting set screen#suspendingScreen=\$(</tmp/ fenrirSuspend)" | socat - UNIX-CLIENT:/tmp/fenrirscreenreader-deamon.sock;;
+    *) /usr/lib/F123-wrappers/configure-email.sh
+esac
+
+exit 0
