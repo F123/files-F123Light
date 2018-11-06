203f0eb662cf4e3a297f698fc8f0600aa299c8ea
Storm Dragon <stormdragon2976@gmail.com>
2018-11-06 13:20:34 -0500

    Overlooked some missing brackets.

--- files/usr/lib/F123-wrappers/mud-loader.sh
+++ files/usr/lib/F123-wrappers/mud-loader.sh
@@ -3,7 +3,7 @@
 mudURL="$1"
 mutName="${mudURL##*/}"
 mudName="${mudName%.git}"
-if [[ !-d "$HOME/$mudName" ; then 
+if [[ !-d "$HOME/$mudName" ]]; then 
     git clone -C "$HOME" "$mudURL" | dialog --progressbox "Downloading and installing game files, please wait..." 0 0
 else
     git clone -C "$HOME/$mudName" "$mudURL" | dialog --progressbox "Making sure everything is up to date, please wait..." 0 0
