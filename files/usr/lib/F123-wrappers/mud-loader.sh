46acde56c50df9cf70e28bc4fb96de66deaa0e60
Storm Dragon <stormdragon2976@gmail.com>
2018-11-06 13:32:05 -0500

    I keep missing simple stuff, I think I'm tired lol.

--- files/usr/lib/F123-wrappers/mud-loader.sh
+++ files/usr/lib/F123-wrappers/mud-loader.sh
@@ -4,11 +4,12 @@ mudURL="$1"
 mudName="${mudURL##*/}"
 mudName="${mudName%.git}"
 if [[ ! -d "$HOME/$mudName" ]]; then 
-    git clone -C "$HOME" "$mudURL" | dialog --progressbox "Downloading and installing game files, please wait..." 0 0
+    git -C "$HOME" clone "$mudURL" | dialog --progressbox "Downloading and installing game files, please wait..." 0 0
 else
-    git clone -C "$HOME/$mudName" "$mudURL" | dialog --progressbox "Making sure everything is up to date, please wait..." 0 0
+    git -C "$HOME/$mudName" pull "$mudURL" | dialog --progressbox "Making sure everything is up to date, please wait..." 0 0
 fi
 
-cd $mudName
+cd "$HOME/$mudName"
 mudLauncher="$(ls -1 *.tin)"
+tt++ $mudLauncher
 exit 0
