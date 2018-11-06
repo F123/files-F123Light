1e70af172c038b090226cd9ceabc25c7c2dc7b94
Storm Dragon <stormdragon2976@gmail.com>
2018-11-06 13:12:36 -0500

    Games can be loaded dynamically so long as they losely follow the module pattern.

---
+++ files/usr/lib/F123-wrappers/mud-loader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mudURL="$1"
+mutName="${mudURL##*/}"
+mudName="${mudName%.git}"
+if [[ !-d "$HOME/$mudName" ; then 
+    git clone -C "$HOME" "$mudURL" | dialog --progressbox "Downloading and installing game files, please wait..." 0 0
+else
+    git clone -C "$HOME/$mudName" "$mudURL" | dialog --progressbox "Making sure everything is up to date, please wait..." 0 0
+fi
+
+cd $mudName
+mudLauncher="$(ls -1 *.tin)"
+exit 0
