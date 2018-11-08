04f75121f6afd9cc3851a674b2d22de70dc482b8
Kyle <kyle@free2.ml>
2018-11-08 11:23:22 -0500

    Moved incremental updates to files/scripts/updates.sh and source this file from update-f123light. Fixed some general formatting issues in the new update helper and explain the basics in the new script's comments.

---
+++ files/scripts/updates.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright 2018, F123 Consulting, <information@f123.org>
+# Copyright 2018, Kyle, <kyle@free2.ml>
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+#--code--
+
+# This is a helper script for update-f123light to perform incremental updates based on a timestamp
+# It will not be installed to a running system
+#
+# Incremental updates are only to be performed if their timestamp is later than
+# /etc/timestamp-F123Light on the system being updated.
+# Each update should be formatted as
+#
+# update yymmddhhmm && {
+# 	do-something here;
+# 	do-something-else here;
+# }
+#
+# Any new updates should be placed at the end of this file for best readability
+# It is recommended to skip a line between updates, also for readability
+
+update 1811072200 && {
+    # Install Pianobar Pandora client on builds last updated before 7 November 2018
+    sudo pacman -S pianobar-git --noconfirm --needed &> /dev/null;
+}
